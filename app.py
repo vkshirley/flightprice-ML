a44379a81cae99888b885e91cf8e94c1b61bb131
Vaibhav Kumar <vk@somaiya.edu>
2022-12-11 16:36:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -207,20 +207,7 @@ def predict():
             Vistara_Premium_economy = 0
             Trujet = 0
 
-        # print(Jet_Airways,
-        #     IndiGo,
-        #     Air_India,
-        #     Multiple_carriers,
-        #     SpiceJet,
-        #     Vistara,
-        #     GoAir,
-        #     Multiple_carriers_Premium_economy,
-        #     Jet_Airways_Business,
-        #     Vistara_Premium_economy,
-        #     Trujet)
-
-        # Source
-        # Banglore = 0 (not in column)
+        
         Source = request.form["Source"]
         if (Source == 'Delhi'):
             s_Delhi = 1
@@ -252,13 +239,7 @@ def predict():
             s_Mumbai = 0
             s_Chennai = 0
 
-        # print(s_Delhi,
-        #     s_Kolkata,
-        #     s_Mumbai,
-        #     s_Chennai)
-
-        # Destination
-        # Banglore = 0 (not in column)
+       
         Source = request.form["Destination"]
         if (Source == 'Cochin'):
             d_Cochin = 1
@@ -267,13 +248,7 @@ def predict():
             d_Hyderabad = 0
             d_Kolkata = 0
         
-        # elif (Source == 'Delhi'):
-        #     d_Cochin = 0
-        #     d_Delhi = 1
-        #     d_New_Delhi = 0
-        #     d_Hyderabad = 0
-        #     d_Kolkata = 0
-
+      
         elif (Source == 'New_Delhi'):
             d_Cochin = 0
             d_Delhi = 0
@@ -302,25 +277,7 @@ def predict():
             d_Hyderabad = 0
             d_Kolkata = 0
 
-        # print(
-        #     d_Cochin,
-        #     d_Delhi,
-        #     d_New_Delhi,
-        #     d_Hyderabad,
-        #     d_Kolkata
-        # )
-        
-
-    #     ['Total_Stops', 'Journey_day', 'Journey_month', 'Dep_hour',
-    #    'Dep_min', 'Arrival_hour', 'Arrival_min', 'Duration_hours',
-    #    'Duration_mins', 'Airline_Air India', 'Airline_GoAir', 'Airline_IndiGo',
-    #    'Airline_Jet Airways', 'Airline_Jet Airways Business',
-    #    'Airline_Multiple carriers',
-    #    'Airline_Multiple carriers Premium economy', 'Airline_SpiceJet',
-    #    'Airline_Trujet', 'Airline_Vistara', 'Airline_Vistara Premium economy',
-    #    'Source_Chennai', 'Source_Delhi', 'Source_Kolkata', 'Source_Mumbai',
-    #    'Destination_Cochin', 'Destination_Delhi', 'Destination_Hyderabad',
-    #    'Destination_Kolkata', 'Destination_New Delhi']
+    
         
         prediction=model.predict([[
             Total_stops,
